1b2a77cae3bdab6336b650e51eb1bf896d9f5491
Crosimo <jean.deborsu@hotmail.com>
2021-08-02 16:28:52 +0200

    serie 2

---
+++ resources/views/bread.blade.php
@@ -0,0 +1,9 @@
+
+
+  <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="{{route('uno')}}">What</a></li>
+      <li class="breadcrumb-item"><a href="{{route('dos')}}">Are</a></li>
+      <li class="breadcrumb-item active" aria-current="page"><a href="{{route('home')}}"> You?</a></li>
+    </ol>
+  </nav>
